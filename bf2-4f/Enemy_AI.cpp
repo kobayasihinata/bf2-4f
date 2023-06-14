e1b126f54c8a426a89f9a395fce13133f49012c5
taiki0627 <104492163+taiki0627@users.noreply.github.com>
2023-06-14 15:03:54 +0900

    EnemyAI

---
+++ bf2-4f/Enemy_AI.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "Enemy_AI.h"
+
+Enemy_AI::Enemy_AI()
+{
+	// やること:プレイヤー座標と敵の座標を更新
+	P_x = 100;
+	P_y = 100;
+	E_x = 200;
+	E_y = 200;
+	// パターンのリセット
+	pattern = 0;
+}
+Enemy_AI::~Enemy_AI()
+{
+
+}
+void Enemy_AI::Update()
+{
+    // プレイヤーがどの位置にいるか(x座標)
+	if (E_x > P_x) {
+		pattern += 0;
+	}
+	else
+	{
+		pattern += 1;
+	}
+
+	// プレイヤーがどの位置にいるか(y座標)
+	if (E_y >= P_y) {
+		pattern += 0;
+	}
+	else
+	{
+		pattern += 2;
+	}
+
+	// プレイヤーが真下にいるなら回避行動をとる
+	if (E_x >= P_x - 30 && E_x <= P_x + 30 && E_y >= P_y && E_y <= P_y + 50)
+	{
+		pattern = 4;
+	}
+
+	//パターンを返す
+	return;
+}
