5d7046e290a116bfb84334f03c02b05879d35f84
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2023-07-06 13:12:17 +0900

    プレイヤーアニメーション調整

--- bf2-4f/Define.h
+++ bf2-4f/Define.h
@@ -5,7 +5,7 @@
 #define SCREEN_WIDTH 640	//画面横幅
 #define SCREEN_HEIGHT 480	//画面高さ
 
-#define PLAYER_HEIGHT 58
+#define PLAYER_HEIGHT 50
 #define PLAYER_WIDTH  32
 
 #define PLAYER_BALLOON_HEIGHT  25	//プレイヤーの風船の高さ
