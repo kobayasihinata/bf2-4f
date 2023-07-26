11b74b0c716a043feab06cfade5515b84f081eee
kanekumasanosuke <122763496+kanekumasanosuke@users.noreply.github.com>
2023-07-26 09:21:50 +0900

    0

--- bf2-4f/UI.cpp
+++ bf2-4f/UI.cpp
@@ -6,7 +6,11 @@
  {
 	UI_image1 = LoadGraph("images/UI/UI_HiScore.png");
 	UI_image2 = LoadGraph("images/UI/UI_Score.png");
-	UI_image3 = LoadDivGraph("images/UI/UI_NumAnimation.png", 10, 10, 1, 320, 32, UI_image);
+	for (int i = 0; i < 10; i++)
+	{
+		UI_image3[i] = NULL;
+	}
+	LoadDivGraph("images/UI/UI_NumAnimation.png", 10, 10, 1, 32, 32, UI_image3);
 	Score_image = LoadGraph("images/Score/GetScore_500.png");
 	Score_image = LoadGraph("images/Score/GetScore_750.png");
 	Score_image = LoadGraph("images/Score/GetScore_1000.png");
@@ -18,7 +22,8 @@
   {
 	  DrawGraph(300, 5, UI_image1, TRUE);
 	  DrawGraph(170, 5, UI_image2, TRUE);
-	  DrawGraph(170, 5, UI_image3, TRUE);
+	  DrawGraph(190, 0, UI_image3[0], TRUE);
+
 	  
   }
 
--- bf2-4f/UI.h
+++ bf2-4f/UI.h
@@ -12,9 +12,8 @@ public:
 	void Draw()const;
 	int UI_image1;
 	int UI_image2;
-	int UI_image3;
-	int Score_image;
-	int UI_image[10];   
+	int UI_image3[10];
+	int Score_image;  
 
 	void AddScore(int point) { gScore += point; }
 	int GetScore() { return gScore;}
