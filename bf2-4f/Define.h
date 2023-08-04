17f0cddd8af2371fc1f5e44fbe7419fb967a05ae
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2023-08-04 11:08:20 +0900

    プレイヤー速度

--- bf2-4f/Define.h
+++ bf2-4f/Define.h
@@ -20,7 +20,7 @@
 #define FALL_SPPED 0.014f			//最高落下速度
 #define MOVE_SPPED 0.009f			//最高移動速度
 #define RISE_SPPED 0.027f			//最高上昇速度
-#define LAND_SPEED 0.020f			//最高移動速度（地面）
+#define LAND_SPEED 0.025f			//最高移動速度（地面）
 	
 #define SEA_SURFACE 445				//海面
 #define UNDER_WATER 470				//海のなか
